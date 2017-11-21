0195d3375963794588c578d01de631067496f116
Jonathan Cameron <jonathan.cameron6@gmail.com>
2017-11-21 09:51:12 -0700

    Add time html, css fixes, js fixes

--- static/temperature.js
+++ static/temperature.js
@@ -223,9 +223,9 @@ $(function() {
 		$('[data-low]').html(response.query.results.channel.item.forecast[0].low);
 		$('[data-forecast-current-with-wind] span').html(response.query.results.channel.wind.chill);
 
-		var className = conditions[response.query.results.channel.item.forecast[0].text];
+		var className = conditions[response.query.results.channel.item.condition.text];
 		$('.forecast-temp-condition-value .condition').addClass(className);
-		$('[data-condition-text]').html(response.query.results.channel.item.forecast[0].text);
+		$('[data-condition-text]').html(response.query.results.channel.item.condition.text);
                 console.log(response);
                 setTimeout(_getWeather, parseInt(response.query.results.channel.ttl) * 1000);
             }
